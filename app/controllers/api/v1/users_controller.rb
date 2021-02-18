95b831241ae8ff51fb2423fc4627f1bbc0a5d1e9
MisterAnthropy <tjcroley@outlook.com>
2021-02-18 15:41:21 -0800

    create method for users and session login method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,4 +5,27 @@ class Api::V1::UsersController < ApplicationController
        render json: @users
     end
     
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: {
+                message: "Login Success",
+                data: user
+            }
+        else
+            render json: {
+                message: "Login Failed",
+                data: "Please Try Again"
+            }
+        end
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+    
 end
