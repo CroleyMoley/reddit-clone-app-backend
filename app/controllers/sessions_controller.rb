457e1dd1e705c125b816dbd267f01dd89e98f2c2
MisterAnthropy <tjcroley@outlook.com>
2021-02-19 10:12:04 -0800

    created sessions controller and set up routes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+module Api
+    module V1
+        class SessionsController < ApplicationController
+
+            def 
+                
+            end
+
+            def 
+
+            end
+        end
+    end
+end
